7cddde95cfb663df2a73c76f011d79785b0b843f
Gamut <dovan.comm@gmail.com>
2025-08-12 16:40:41 +0700

    fix: edit question 3-filter user names that longer than 17 characters

--- question-3.js
+++ question-3.js
@@ -8,7 +8,7 @@ async function displayUser() {
     try {
         const getData = await getUsers()
         const toDisplay = await getData.json()
-        const result = toDisplay.map((data) => data.name)
+        const result = toDisplay.map((data) => data.name).filter((data) => data.length > 17)
         console.log(result)
     } catch (error) {
         console.log(error)
